78fac2e78d7b2f118bc298f83e0184137812b089
AndreLss <andre_luis98@outlook.com>
2020-10-30 12:12:00 -0300

    Adiciona cors validation

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const {
 } = process.env;
 
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
+
+app.use(cors());
 
 const schema = require('./configs/graphql_schema');
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
