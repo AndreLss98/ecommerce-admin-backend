6c8a9006f7036acafdf20fd0495dc3169eaab2c2
AndreLss <andre_luis98@outlook.com>
2020-11-16 12:16:17 -0300

    cria sistema de save de usuario com hash na senha

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,5 +1,6 @@
+const bcrypt = require('bcryptjs');
 const routes = require('express').Router();
-const user = require('../repositorys/user');
+const UserRepository = require('../repositorys/user');
 const authFunctions = require('./../shared/authentication');
 
 const {
@@ -7,23 +8,34 @@ const {
     SESS_LIFETIME
 } = process.env;
 
-const _mockUsers = [
-    { id: 1, name: 'André Luís', email: 'andre@gmail.com', senha: '123456' }
-];
-
 routes.post('/login', async (req, res, next) => {
-    const { email, senha } = req.body;
-    const userExists = _mockUsers.find(user => user.email === email && user.senha === senha);
+    const { Email, Senha } = req.body;
+    const user = await UserRepository.getAdminUser(Email);
 
-    if (userExists) {
-        return res.cookie(SESS_ID, authFunctions.generateJWT({ userID: userExists.id, userName: userExists.name }), {
+    if (!user) return res.status(400).send({ error: "User not found" });
+    if (!await bcrypt.compare(Senha, user.Hash)) return res.status(400).send({ error: "Invalid password" });
+
+    if (user) {
+        return res.cookie(SESS_ID, authFunctions.generateJWT({ userID: user.Id, userName: user.Nome }), {
             maxAge: parseInt(SESS_LIFETIME),
             httpOnly: true,
             sameSite: true,
             secure: false
-        }).status(200).send({ userID: userExists.id, userName: userExists.name });
+        }).status(200).send({ Id: user.Id, Nome: user.Nome });
     } else {
         return res.status(400).send({ error: "Auth Failed" });
+    }
+});
+
+routes.post('/signup', async (req, res, next) => {
+    const { Email, Senha, Nome } = req.body;
+    const checkUser = await UserRepository.getAdminUser(Email);
+    if (checkUser) return res.status(400).send({ error: "User already exists" });
+
+    try {
+        return res.status(200).send(await UserRepository.saveAdminUser({ Email, Senha, Nome }));
+    } catch (trace) {
+        return res.status(400).send({ error: "Register Failed", trace });
     }
 });
 
--- src/repositorys/user.js
+++ src/repositorys/user.js
@@ -1,11 +1,31 @@
 const db = require('./../configs/knex');
+const bcrypt = require('bcryptjs');
 
 const TABLE = 'Customers';
+const ADMIN_TABLE = 'AdminUser';
 
 async function getByEmail(CustomerEmail) {
     return await db(TABLE).where({ CustomerEmail }).first();
 }
 
+async function getAdminUser(Email) {
+    return await db(ADMIN_TABLE).where({ Email }).first();
+}
+
+async function saveAdminUser(user) {
+    user.Hash = await bcrypt.hash(user.Senha, 10);
+    delete user.Senha;
+
+    try {
+        const id = await db(ADMIN_TABLE).insert(user);
+        return await db.select('Id', 'Nome').from(ADMIN_TABLE).where({ Id: id[0] }).first();
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
-    getByEmail
+    getByEmail,
+    getAdminUser,
+    saveAdminUser
 }
