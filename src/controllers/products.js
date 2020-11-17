1ff6d1e065c42b4ae3267dcbd3a1c2641629deac
AndreLss <andre_luis98@outlook.com>
2020-11-17 15:45:43 -0300

    protege rota para salvar/atualizar bundle

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,11 +8,11 @@ router.get('/bundle-products/:idBundle', async (req, res, next) => {
     return res.status(200).send(await ProductsRepository.getProductsOfBundle(req.params.idBundle));
 });
 
-router.get('/bundle/:handle', authMiddleware(), async (req, res, next) => {
+router.get('/bundle/:handle', async (req, res, next) => {
     return res.status(200).send(await ProductsRepository.getBundle(req.params.handle));
 });
 
-router.post('/bundle', async (req, res, next) => {
+router.post('/bundle', authMiddleware(), async (req, res, next) => {
     const { BundleID, Products } = req.body;
     ProductsRepository.save(BundleID, Products);
     return res.status(200).send({ response: 'Ok' });
