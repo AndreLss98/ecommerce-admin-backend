007e7b2a471c32fd2f86eff29e1474efc08cb868
AndreLss <andre_luis98@outlook.com>
2020-11-24 14:31:29 -0300

    remove filtro de creditos usados igual a 1

--- src/repositorys/credits.js
+++ src/repositorys/credits.js
@@ -3,12 +3,12 @@ const db = require('./../configs/knex');
 const TABLE = 'CreditUsageLog';
 
 async function getAllByCustomerID(CustomerID) {
-    return await db(TABLE).where({ CustomerID, CreditsUsed: 1 }).orderBy('UsageDate', 'desc');
+    return await db(TABLE).where({ CustomerID }).orderBy('UsageDate', 'desc');
 }
 
 async function getAllInInteval(startDate, endDate) {
-    let query = endDate && startDate !== endDate? db(TABLE).andWhere({ CreditsUsed: 1 }).whereBetween('UsageDate', [startDate, endDate]) :
-        db(TABLE).where('UsageDate', '>=', startDate).andWhere({ CreditsUsed: 1 });
+    let query = endDate && startDate !== endDate? db(TABLE).whereBetween('UsageDate', [startDate, endDate]) :
+        db(TABLE).where('UsageDate', '>=', startDate);
     return await query.orderBy('UsageDate', 'desc');
 }
 
