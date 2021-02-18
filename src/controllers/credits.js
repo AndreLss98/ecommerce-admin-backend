bc4555278c806e6191a3d95277260ef23577cbe5
ANDRÉ <andreluiss@ufg.br>
2021-02-17 22:48:57 -0300

    finaliza endpoint para alterar produto da conta do usuario

--- src/controllers/credits.js
+++ src/controllers/credits.js
@@ -1,5 +1,9 @@
 const bunyam = require('bunyan');
-const routes = require('express').Router();
+const router = require('express').Router();
+
+const authMiddleware = require('./../middlewares/authentication');
+
+const CreditRepository = require('./../repositorys/credits');
 
 const log = bunyam.createLogger({
     name: 'Credits',
@@ -15,4 +19,14 @@ const log = bunyam.createLogger({
     ]
 });
 
-module.exports = app => app.use('/credits', routes);
+router.put('/alter-plugin', async (req, res, next) => {
+    const { email, oldPlugin, newPlugin } = req.body;
+    if (!email) return res.status(400).send({ message: "User email is required" });
+    if (!oldPlugin) return res.status(400).send({ message: "Old plugin is required" });
+    if (!newPlugin) return res.status(400).send({ message: "New plugin is required" });
+
+    await CreditRepository.alterPluginUsage(email, oldPlugin, newPlugin);
+    return res.status(200).send(newPlugin);
+});
+
+module.exports = app => app.use('/credits', router);
--- src/repositorys/credits.js
+++ src/repositorys/credits.js
@@ -14,7 +14,27 @@ async function getAllInInteval(startDate, endDate, itemID) {
     return await query.orderBy('UsageDate', 'desc');
 }
 
+async function alterPluginUsage(email, oldPlugin, newPlugin) {
+    try {
+        oldPlugin = parseInt(oldPlugin);
+
+        const changeLink = await db.select(['DL.LinkID']).from('DownloadLinks AS DL')
+            .innerJoin('Customers AS C', 'C.ShopifyCustomerNumber', 'DL.Customer')
+            .where('C.CustomerEmail', '=', email).andWhere('DL.ItemID', '=', oldPlugin).orderBy('DL.LastAttempt', 'DESC').first();
+
+        const changeLog = await db.select(['CUL.CreditUsageID']).from(`${TABLE} AS CUL`)
+            .innerJoin('Customers AS C', 'C.ShopifyCustomerNumber', 'CUL.CustomerID')
+            .where('C.CustomerEmail', '=', email).andWhere('CUL.ItemID', '=', oldPlugin).orderBy('CUL.UsageDate', 'DESC').first();
+
+        await db('DownloadLinks').update({ ItemID: newPlugin.id, ItemTitle: newPlugin.title }).where({ LinkID: changeLink.LinkID });
+        await db(TABLE).update({ ItemID: newPlugin.id, ItemTitle: newPlugin.title }).where({ CreditUsageID: changeLog.CreditUsageID });
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
+    getAllInInteval,
+    alterPluginUsage,
     getAllByCustomerID,
-    getAllInInteval
 }
